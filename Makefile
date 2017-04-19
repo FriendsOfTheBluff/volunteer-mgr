efef1f8f0211fe36b2bed93c9bd9e712c223021c
Luke Bakken <luke@bakken.io>
2017-04-19 07:02:42 -0700

    make rebar3.mk fancier

--- Makefile
+++ Makefile
@@ -1,21 +1,3 @@
-.PHONY: all compile dialyzer rel run test
-
-all:
-	@$(REBAR3) do clean, compile
-
-compile:
-	@$(REBAR3) compile
-
-dialyzer: compile
-	@$(REBAR3) dialyzer
-
-test: compile
-	@$(REBAR3) ct
-
-run:
-	@$(REBAR3) shell
-
-rel:
-	@$(REBAR3) release
+all: clean compile
 
 include rebar3.mk
--- rebar3.mk
+++ rebar3.mk
@@ -1,3 +1,5 @@
+.PHONY: clean compile dialyzer rel run test
+
 REBAR3_URL := https://s3.amazonaws.com/rebar3/rebar3
 
 # If there is a rebar in the current directory, use it
@@ -6,15 +8,43 @@ REBAR3 := $(CURDIR)/rebar3
 endif
 
 # Fallback to rebar on PATH
-REBAR3 ?= $(shell test -x `which rebar3` 2>/dev/null && which rebar3 || echo "$(CURDIR)/rebar3")
+REBAR3 ?= $(shell which rebar3 2>/dev/null || echo "$(CURDIR)/rebar3")
 
 # And finally, prep to download rebar if all else fails
 ifeq ($(REBAR3),)
 REBAR3 := $(CURDIR)/rebar3
 endif
 
+clean: rebar3
+	@$(REBAR3) clean
+
+compile: rebar3
+	@$(REBAR3) compile
+
+dialyzer: compile
+	@$(REBAR3) dialyzer
+
+rel: rebar3
+	@$(REBAR3) release
+
+run: rebar3
+	@$(REBAR3) shell
+
+test: compile
+	@$(REBAR3) ct
+
 rebar3: $(REBAR3)
 
 $(REBAR3):
-	curl -Lo $(REBAR3) $(REBAR3_URL) || wget $(REBAR3_URL)
-	chmod a+x $(REBAR3)
+	@echo "Retrieving $(REBAR3) from $(REBAR3_URL)"
+	@curl -sLo $(REBAR3) $(REBAR3_URL) || wget $(REBAR3_URL)
+	@chmod a+x $(REBAR3)
+	@echo
+	@echo '************************************************************************'
+	@echo 'NOTE - please follow these instructions:'
+	@echo
+	$(CURDIR)/rebar3 local install
+	@echo
+	@echo '************************************************************************'
+	@echo
+	@rm -f $(CURDIR)/rebar3
