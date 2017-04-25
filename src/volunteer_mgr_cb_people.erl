c212b5d8d13b9d7dc31af1a990a470cc58b55f55
Luke Bakken <luke@bakken.io>
2017-04-25 11:57:44 -0700

    pass hard-coded tags into template

--- src/volunteer_mgr_cb_people.erl
+++ src/volunteer_mgr_cb_people.erl
@@ -3,6 +3,7 @@
 -export([init/2]).
 
 init(Req, State) ->
-    {ok, ResponseBody} = volunteer_mgr_templates_people:render(),
+    Data = [{tags, [foo, bar, baz, bat]}],
+    {ok, ResponseBody} = volunteer_mgr_templates_people:render(Data),
     Reply = cowboy_req:reply(200, [{<<"content-type">>, <<"text/html">>}], ResponseBody, Req),
     {ok, Reply, State}.
