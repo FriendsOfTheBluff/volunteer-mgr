c68892c0dc3543bc3b306f0f079ead17a5819c4e
Luke Bakken <luke@bakken.io>
2017-04-26 07:42:33 -0700

    Fix dialyzer error which uncovered a bug in passing Notes to volmgr_db_person:create

--- src/volmgr_cb_people.erl
+++ src/volmgr_cb_people.erl
@@ -2,30 +2,33 @@
 
 -export([init/2]).
 
+-spec init(cowboy_req:req(), term()) -> {ok, cowboy_req:req(), term()}.
 init(Req, State) ->
     Method = cowboy_req:method(Req),
 	HasBody = cowboy_req:has_body(Req),
     Reply = process_request(Method, HasBody, Req),
     {ok, Reply, State}.
 
+-spec process_request(binary(), boolean(), cowboy_req:req()) -> cowboy_req:req().
 process_request(<<"GET">>, _, Req) ->
     render_form_with_existing_tags(Req);
 process_request(<<"POST">>, true, Req) ->
     {ok, PostVals, Req1} = cowboy_req:body_qs(Req),
-    {<<"first_name">>, F} = lists:keyfind(<<"first_name">>, 1, PostVals),
-    {<<"last_name">>, L} = lists:keyfind(<<"last_name">>, 1, PostVals),
+    F = cb_util:get_post_value(<<"first_name">>, PostVals),
+    L = cb_util:get_post_value(<<"last_name">>, PostVals),
     %% TODO {<<"phone">>, P} = lists:keyfind(<<"phone">>, 1, PostVals),
     P = {123, 555, 1212},
-    {<<"email">>, E} = lists:keyfind(<<"email">>, 1, PostVals),
-    {<<"notes">>, N} = lists:keyfind(<<"notes">>, 1, PostVals),
+    E = cb_util:get_post_value(<<"email">>, PostVals),
+    N = cb_util:get_post_value(<<"notes">>, PostVals),
     Tags = [binary_to_atom(T, latin1) || {<<"tags">>, T} <- PostVals],
-    ok = volmgr_db_people:create(F, L, P, E, N, Tags),
+    ok = volmgr_db_people:create(F, L, P, E, [N], Tags),
     render_form_with_existing_tags(Req1);
 process_request(<<"POST">>, false, Req) ->
     cowboy_req:reply(400, [], <<"Missing Body">>, Req);
 process_request(_, _, Req) ->
 	cowboy_req:reply(405, Req).
 
+-spec render_form_with_existing_tags(cowboy_req:req()) -> cowboy_req:req().
 render_form_with_existing_tags(Req) ->
     Tags = volmgr_db_tags:retrieve(),
     Data = [{tags, [T || {T, _} <- Tags]}],
--- src/volmgr_cb_tags.erl
+++ src/volmgr_cb_tags.erl
@@ -2,19 +2,19 @@
 
 -export([init/2]).
 
--spec init(cowboy_req:req(), term()) -> {ok, any(), any()}.
+-spec init(cowboy_req:req(), term()) -> {ok, cowboy_req:req(), term()}.
 init(Req, State) ->
     Method = cowboy_req:method(Req),
 	HasBody = cowboy_req:has_body(Req),
     Reply = process_request(Method, HasBody, Req),
     {ok, Reply, State}.
 
--spec process_request(binary(), boolean(), cowboy_req:req()) -> {ok, any(), any()}.
+-spec process_request(binary(), boolean(), cowboy_req:req()) -> cowboy_req:req().
 process_request(<<"GET">>, _, Req) ->
     render_existing_tags(Req);
 process_request(<<"POST">>, true, Req) ->
     {ok, PostVals, Req1} = cowboy_req:body_qs(Req),
-    TagBin = proplists:get_value(<<"new_tag">>, PostVals),
+    TagBin = cb_util:get_post_value(<<"new_tag">>, PostVals),
     Tag = binary_to_atom(TagBin, latin1),
     ok = volmgr_db_tags:create(Tag),
     render_existing_tags(Req1);
@@ -23,6 +23,7 @@ process_request(<<"POST">>, false, Req) ->
 process_request(_, _, Req) ->
 	cowboy_req:reply(405, Req).
 
+-spec render_existing_tags(cowboy_req:req()) -> cowboy_req:req().
 render_existing_tags(Req) ->
     Tags = volmgr_db_tags:retrieve(),
     Data = [{tags, [T || {T, _} <- Tags]}],
